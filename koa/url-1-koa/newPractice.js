1c58ff371c6b293e6ceeddfe757a7d73f8cb39bc
SimonZoo <simonzoo@yeah.net>
2017-09-17 14:59:21 +0800

    Rename

--- koa/url-1-koa/newPractice.js
+++ koa/url-1-koa/newPractice.js
@@ -27,7 +27,7 @@ router.post('/signin', async (ctx, next) => {
         name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
+    if (password === `${name}`) {
         ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
     } else {
         ctx.response.body = `<h1>Login failed!</h1>
